dfd0389d076c5f994047a82a38e29f2cb99f531f
colepalm <palm.cole@gmail.com>
2023-12-13 21:04:44 -0700

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import os, random, datetime, requests
+
+def update_image():
+    # Load and display the next image
+    pass
+
+def update_time():
+    # Update and display the current time
+    pass
+
+def update_weather():
+    # Fetch and display the current weather
+    pass
+
+def update_calendar():
+    # Fetch and display calendar events
+    pass
+
+# Set up the main window using Tkinter
+root = tk.Tk()
+root.attributes('-fullscreen', True)
+
+# Set up frames/labels for image, time, weather, calendar
+
+# Start the loop for updating content
+root.mainloop()
