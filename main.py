a8a1c74bf2b19c9f6b726da6497cc74d32978d31
colepalm <palm.cole@gmail.com>
2023-12-20 21:29:20 -0700

    display photo script working with local

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
+import os
+import random
 import tkinter as tk
+
 from PIL import Image, ImageTk
-import os, random, datetime, requests
+
 
 def update_image():
-    # Load and display the next image
-    pass
+    # Get a list of files in the directory
+    photo_dir = './photos'
+    files = [os.path.join(photo_dir, f) for f in os.listdir(photo_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+    if not files:
+        print("No images found in the directory.")
+        return
+
+    # Select a random image file
+    selected_image = random.choice(files)
+
+    # Load the selected image using PIL
+    image = Image.open(selected_image)
+    image = image.resize((800, 600), Image.LANCZOS)
+    photo = ImageTk.PhotoImage(image)
+
+    # Display the image
+    image_label.config(image=photo)
+    image_label.image = photo
+
+
+def update_image_loop():
+    update_image()
+    # Set the timer (e.g., 120 seconds)
+    root.after(120000, update_image_loop)
+
 
 def update_time():
     # Update and display the current time
     pass
 
+
 def update_weather():
     # Fetch and display the current weather
     pass
+
 
 def update_calendar():
     # Fetch and display calendar events
     pass
 
+
 # Set up the main window using Tkinter
 root = tk.Tk()
 root.attributes('-fullscreen', True)
 
-# Set up frames/labels for image, time, weather, calendar
+# Set up a label for the image
+image_label = tk.Label(root)
+image_label.pack()
+
+update_image_loop()
 
 # Start the loop for updating content
 root.mainloop()
