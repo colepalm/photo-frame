94231a0b885c395e8865b83e109709b9671ba889
colepalm <palm.cole@gmail.com>
2024-04-27 13:58:20 -0600

    fixes scaling issues

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import sys
 from datetime import datetime
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtGui import QPixmap, QImage
 
 
 class PhotoFrameApp(QMainWindow):
@@ -13,36 +13,44 @@ class PhotoFrameApp(QMainWindow):
         self.setWindowTitle("Digital Photo Frame")
         self.showFullScreen()
 
-        # Main widget and layout
-        self.centralWidget = QWidget(self)
-        self.setCentralWidget(self.centralWidget)
-        self.layout = QVBoxLayout()
-        self.centralWidget.setLayout(self.layout)
+        # Create a central widget and set a main vertical layout
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        main_layout = QVBoxLayout(central_widget)
+        main_layout.setContentsMargins(10, 10, 10, 10)
+        main_layout.setSpacing(0)
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_image)
 
         # Image label
-        self.imageLabel = QLabel()
-        self.layout.addWidget(self.imageLabel)
+        self.image_label = QLabel()
+        self.image_label.setScaledContents(True)
+        main_layout.addWidget(self.image_label, 1)
 
-        # Update image every 2 minutes
-        self.imageUpdateTimer = QTimer(self)
-        self.imageUpdateTimer.timeout.connect(self.update_image)
-        self.imageUpdateTimer.start(120000)  # 2 minutes in milliseconds
+        # Horizontal layout for the bottom right time display
+        bottom_layout = QHBoxLayout()
+        bottom_layout.addStretch(1)  # Pushes the time label to the right
+        self.time_label = QLabel("00:00", self)
+        self.time_label.setFont(QFont('Arial', 18))
+        self.time_label.setStyleSheet("color: white; background-color: rgba(0, 0, 0, 100);")
+        bottom_layout.addWidget(self.time_label)
 
-        self.timeLabel = QLabel("00:00:00")
-        self.layout.addWidget(self.timeLabel)
-        self.timeUpdateTimer = QTimer(self)
-        self.timeUpdateTimer.timeout.connect(self.update_time)
-        self.timeUpdateTimer.start(1000)  # 1 second in milliseconds
+        main_layout.addLayout(bottom_layout)
 
-        # Weather label
-        self.weatherLabel = QLabel("Loading weather...")
-        self.layout.addWidget(self.weatherLabel)
-        self.weatherUpdateTimer = QTimer(self)
-        self.weatherUpdateTimer.timeout.connect(self.update_weather)
-        self.weatherUpdateTimer.start(600000)  # 10 minutes in milliseconds
+        # Top right corner for weather information
+        top_layout = QHBoxLayout()
+        top_layout.addStretch(1)  # Pushes the weather label to the right
+        self.weather_label = QLabel("Loading weather...")
+        self.weather_label.setFont(QFont('Arial', 16))
+        self.weather_label.setStyleSheet("color: white; background-color: rgba(0, 0, 0, 100);")
+        main_layout.addLayout(top_layout)
+        top_layout.addWidget(self.weather_label)
+
+        # TODO: Requery weather periodically
+        # self.weatherUpdateTimer = QTimer(self)
+        # self.weatherUpdateTimer.timeout.connect(self.update_weather)
+        # self.weatherUpdateTimer.start(600000)  # 10 minutes in milliseconds
 
         self.photos_dir = './photos'
         self.photos_list = self.load_photos()
@@ -65,8 +73,13 @@ class PhotoFrameApp(QMainWindow):
 
         photo_path = self.photos_list[self.current_photo]
         pixmap = QPixmap(photo_path)
-        self.imageLabel.setPixmap(pixmap)
 
+        # Scale the pixmap to fit the label while maintaining the aspect ratio
+        scaled_pixmap = pixmap.scaled(self.image_label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+        self.image_label.setPixmap(scaled_pixmap)
+
+        # Cycle through the photo list
         self.current_photo += 1
         if self.current_photo >= len(self.photos_list):
             self.current_photo = 0
